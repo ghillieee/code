03d8bee979637803970ad23acab8abb07c48b1eb
ghillie <88168239+ghillieee@users.noreply.github.com>
2022-01-17 09:35:53 +0000

    Delete teleport menu

--- FiveM Files/TeleportMenu/sv_combat.lua
+++
@@ -1,6 +0,0 @@
-RegisterNetEvent("K9:sv_combatSetEntityAlpha")
-AddEventHandler("K9:sv_combatSetEntityAlpha", function(value)
-    local source = source
-    local entity = GetPlayerPed(source)
-    TriggerClientEvent("K9:cl_combatSetEntityAlpha", -1, entity, value)
-end)
